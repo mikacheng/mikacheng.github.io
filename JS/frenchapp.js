6d258520482943e2745dcbf7e1d913bfdd87201c
mikacheng <contactmickael@yahoo.fr>
2023-07-11 22:58:49 +0100

    added new files

---
+++ JS/frenchapp.js
@@ -0,0 +1,121 @@
+function bleu() {
+  var audio = new Audio('sounds/bleu.mp3');
+  audio.play();
+} 
+function gris() {
+  var audio = new Audio('sounds/gris.mp3');
+  audio.play();
+} 
+function jaune() {
+  var audio = new Audio('sounds/jaune.mp3');
+  audio.play();
+} 
+function marron() {
+  var audio = new Audio('sounds/marron.mp3');
+  audio.play();
+} 
+function noir() {
+  var audio = new Audio('sounds/noir.mp3');
+  audio.play();
+} 
+function orange() {
+  var audio = new Audio('sounds/orange.mp3');
+  audio.play();
+} 
+function rose() {
+  var audio = new Audio('sounds/rose.mp3');
+  audio.play();
+} 
+function rouge() {
+  var audio = new Audio('sounds/rouge.mp3');
+  audio.play();
+} 
+function vert() {
+  var audio = new Audio('sounds/vert.mp3');
+  audio.play();
+} 
+function violet() {
+  var audio = new Audio('sounds/violet.mp3');
+  audio.play();
+} 
+function blanc() {
+  var audio = new Audio('sounds/blanc.mp3');
+  audio.play();
+} 
+
+function chambre() {
+  var audio = new Audio('sounds/une-chambre.mp3');
+  audio.play();
+} 
+
+function lit() {
+  var audio = new Audio('sounds/un-lit.mp3');
+  audio.play();
+}
+
+function reveille() {
+  var audio = new Audio('sounds/un-reveille.mp3');
+  audio.play();
+}
+
+function lampe() {
+  var audio = new Audio('sounds/une-lampe.mp3');
+  audio.play();
+}
+
+function fenêtre() {
+  var audio = new Audio('sounds/une-fenêtre.mp3');
+  audio.play();
+} 
+
+function fauteuil() {
+  var audio = new Audio('sounds/un-fauteuil.mp3');
+  audio.play();
+}
+
+function cheminée() {
+  var audio = new Audio('sounds/une-cheminée.mp3');
+  audio.play();
+}
+
+function canapé() {
+  var audio = new Audio('sounds/un-canapé.mp3');
+  audio.play();
+}
+
+function salon() {
+  var audio = new Audio('sounds/un-salon.mp3');
+  audio.play();
+}
+
+function évier() {
+  var audio = new Audio('sounds/un-évier.mp3');
+  audio.play();
+}
+
+function cuisine() {
+  var audio = new Audio('sounds/une-cuisine.mp3');
+  audio.play();
+}
+
+function four() {
+  var audio = new Audio('sounds/un-four.mp3');
+  audio.play();
+}
+
+function frigo() {
+  var audio = new Audio('sounds/un-frigo.mp3');
+  audio.play();
+}
+
+function placard() {
+  var audio = new Audio('sounds/un-placard.mp3');
+  audio.play();
+}
+
+
+
+
+
+
+
